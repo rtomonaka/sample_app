d1164d14ae0876bb1954d4981e126ce5edf77622
tomonaka <rtmakerobots@gmail.com>
2017-09-13 07:30:55 +0000

    fin cahp7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    #debugger
   end
+  
+  def new
+    @user = User.new
+  end
+  
+    
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #sucsess
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user       # <- redirect_to user_path(@user.id)
+    else
+      #failer
+      render 'new'
+    end
+  end
+  
+  
+    private
+    
+      def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                     :password_confirmation)
+      end
 end
